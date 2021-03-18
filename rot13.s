6938d90fa5c020cb4659e4c153ac81bc04af2dd5
Uladzimir Kaviaka <257276@student.pwr.edu.pl>
2021-03-18 17:32:33 +0100

    Add output and input functions

--- rot13.s
+++ rot13.s
@@ -1,8 +1,8 @@
 SYSEXIT32 = 1
 SYSREAD = 3
 SYSWRITE = 4
-STDIN = 0
-STDOUT = 1
+STDIN = 0       		#deskryptor SYSREAD
+STDOUT = 1				#deskryptor SYSWRITE
 SYSCALL = 0x80
 SUCCESSEDEXIT = 0
 
@@ -10,10 +10,25 @@ input_length = 100
 
 .global _start
 
+.bss
+input: .space input_len #zarezerwowanie w pamieci ciagu pustego o dlugosci 100
+
 .data
 msg_start: .ascii "Podaj zdanie: "
 msg_start_length = . - msg_start
 
 msg_rot13: .ascii "Rot13: "
 msg_rot13_length = . - msg_rot13
+
+mov $SYSWRITE, %eax
+mov $STDOUT, %ebx
+mov $msg_start, %ecx
+mov $msg_start_length, %edx
+int $SYSCALL
+
+mov $SYSREAD, %eax
+mov $STDIN, %ebx
+mov $input, %ecx
+mov $input_length, %edx
+int $SYSCALL
 
