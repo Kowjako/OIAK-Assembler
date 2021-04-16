ff00ab6dcbab2078991106ab4eb5e2b21d92cd4e
Uladzimir Kaviaka <257276@student.pwr.edu.pl>
2021-04-16 15:30:05 +0200

    Add start inputs and variable initialization

--- Laboratorium2/kalkulator.s
+++ Laboratorium2/kalkulator.s
@@ -1,88 +1,85 @@
-SYSEXIT32 = 1
-SYSREAD = 3
-SYSWRITE = 4
-STDIN = 0       		#deskryptor SYSREAD
-STDOUT = 1				#deskryptor SYSWRITE
-SYSCALL = 0x80
-SUCCESSEDEXIT = 0
-
-input_length = 100
-operation_length = 2
-
-.global _start
-
-.bss
-firstNumber: .space input_length 
-secondNumber: .space input_length
-operationNumber: .space operation_length
-roundNumber: .space operation_length
-
 .data
-msg_first: .ascii "Wpisz pierwsza liczbe:\n"
-msg_first_length = . - msg_first
 
-msg_second: .ascii "Wpisz druga liczbe:\n"
-msg_second_length = . - msg_second
+format_s: .ascii "%s\0"
+format_f: .ascii "%f\0"
+format_d: .ascii "%d\0"
 
-msg_operation: .ascii "Wybierz operacje:\n1-dodawanie, 2-odejmowanie, 3-mnozenie, 4-dzielenie\n"
-msg_operation_length = . - msg_operation
+a: .space 100
+b: .space 100
+trash: .double 0
+operation: .space 1
+round: .space 1
+result: .space 32
 
-msg_round: .ascii "Wybierz typ zaokraglania:\n1-najblizsze, 2-w gore, 3-w dol, 4-do zera\n"
-msg_round_length = . - msg_round
+msg_first: .ascii "Wpisz pierwsza liczbe:\n\0"
+msg_second: .ascii "Wpisz druga liczbe:\n\0"
+msg_operation: .ascii "Wybierz operacje:\n1-dodawanie, 2-odejmowanie, 3-mnozenie, 4-dzielenie\n\0"
+msg_round: .ascii "Wybierz typ zaokraglania:\n1-najblizsze, 2-w gore, 3-w dol, 4-do zera\n\0"
+
+najblizse: .short 0x000
+wdol: .short 0x400
+wgore: .short 0x800
+obciecie: .short 0xC00
+
+
+FPUControlWord: .short 0x037f
 
 .text
+	.globl main
 
-_start:
-mov $SYSWRITE, %eax
-mov $STDOUT, %ebx
-mov $msg_first, %ecx
-mov $msg_first_length, %edx
-int $SYSCALL
+main:
+	call readData
+	call doAction
+	call finish
 
-mov $SYSREAD, %eax
-mov $STDIN, %ebx
-mov $firstNumber, %ecx
-mov $input_length, %edx
-int $SYSCALL
+readData:
+mov $0, %eax
+mov $format_s, %edi
+mov	$msg_first, %esi
+call printf
 
-mov $SYSWRITE, %eax
-mov $STDOUT, %ebx
-mov $msg_second, %ecx
-mov $msg_second_length, %edx
-int $SYSCALL
+mov $0, %eax
+mov $format_f, %edi
+mov $a, %esi
+call scanf
 
-mov $SYSREAD, %eax
-mov $STDIN, %ebx
-mov $secondNumber, %ecx
-mov $input_length, %edx
-int $SYSCALL
+mov $0, %eax
+mov $format_s, %edi
+mov	$msg_second, %esi
+call printf
 
-mov $SYSWRITE, %eax
-mov $STDOUT, %ebx
-mov $msg_operation, %ecx
-mov $msg_operation_length, %edx
-int $SYSCALL
+mov $0, %eax
+mov $format_f, %edi
+mov $b, %esi
+call scanf
 
-mov $SYSREAD, %eax
-mov $STDIN, %ebx
-mov $operationNumber, %ecx
-mov $operation_length, %edx
-int $SYSCALL
+mov $0, %eax
+mov $format_s, %edi
+mov	$msg_operation, %esi
+call printf
 
-mov $SYSWRITE, %eax
-mov $STDOUT, %ebx
-mov $msg_round, %ecx
-mov $msg_round_length, %edx
-int $SYSCALL
+mov $0, %eax
+mov $format_d, %edi
+mov $operation, %esi
+call scanf
 
-mov $SYSREAD, %eax
-mov $STDIN, %ebx
-mov $roundNumber, %ecx
-mov $operation_length, %edx
-int $SYSCALL
+mov $0, %eax
+mov $format_s, %edi
+mov	$msg_round, %esi
+call printf
 
+mov $0, %eax
+mov $format_d, %edi
+mov $round, %esi
+call scanf
+ret
 
-#Wysjcie z programu
-mov $SYSEXIT32, %eax
-mov $SUCCESSEDEXIT, %ebx
-int $SYSCALL
+doAction:
+#finit
+#fldcw FPUControlWord
+#flds a
+#fadds b
+
+finish:
+mov $0, %eax
+call exit
