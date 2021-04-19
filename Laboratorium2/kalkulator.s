6f6497b33c31bf15dcbd6ed4f31b6796a6108683
Uladzimir Kaviaka <257276@student.pwr.edu.pl>
2021-04-19 21:05:35 +0200

    Rewrited start initialization

--- Laboratorium2/kalkulator.s
+++ Laboratorium2/kalkulator.s
@@ -1,85 +1,23 @@
+.global main
+
 .data
+    msg1: .ascii "Please input first double\n\0"
+    msg2: .ascii "Please input second double\n\0"
+    menu: .ascii "Please input operation (int)\n1. Add\n2. Sub\n3. Div\n4. Mult\n\0"
+    control: .ascii "Please input rouding (int)\n1. Cut\n2. Up\n3. Down\n4. Nearest\n\0"
+    output: .ascii "Your output\n\0"
 
-format_s: .ascii "%s\0"
-format_f: .ascii "%f\0"
-format_d: .ascii "%d\0"
+    first_d: .double 1.0
+    second_d: .double 2.0
+    output_d: .double 0.0
 
-a: .space 100
-b: .space 100
-trash: .double 0
-operation: .space 1
-round: .space 1
-result: .space 32
+    operation: .int 0
+    control_operation: .int 0
 
-msg_first: .ascii "Wpisz pierwsza liczbe:\n\0"
-msg_second: .ascii "Wpisz druga liczbe:\n\0"
-msg_operation: .ascii "Wybierz operacje:\n1-dodawanie, 2-odejmowanie, 3-mnozenie, 4-dzielenie\n\0"
-msg_round: .ascii "Wybierz typ zaokraglania:\n1-najblizsze, 2-w gore, 3-w dol, 4-do zera\n\0"
+    format_d: .ascii "%lf\0"
+    format_s: .ascii "%s\0"
+    format_i: .ascii "%i\0"
 
-najblizse: .short 0x000
-wdol: .short 0x400
-wgore: .short 0x800
-obciecie: .short 0xC00
-
-
-FPUControlWord: .short 0x037f
-
+    control_word: .short 0x037f #standardowa wartosc z instrukcji
+    endline: .ascii "\n\0"
 .text
-	.globl main
-
-main:
-	call readData
-	call doAction
-	call finish
-
-readData:
-mov $0, %eax
-mov $format_s, %edi
-mov	$msg_first, %esi
-call printf
-
-mov $0, %eax
-mov $format_f, %edi
-mov $a, %esi
-call scanf
-
-mov $0, %eax
-mov $format_s, %edi
-mov	$msg_second, %esi
-call printf
-
-mov $0, %eax
-mov $format_f, %edi
-mov $b, %esi
-call scanf
-
-mov $0, %eax
-mov $format_s, %edi
-mov	$msg_operation, %esi
-call printf
-
-mov $0, %eax
-mov $format_d, %edi
-mov $operation, %esi
-call scanf
-
-mov $0, %eax
-mov $format_s, %edi
-mov	$msg_round, %esi
-call printf
-
-mov $0, %eax
-mov $format_d, %edi
-mov $round, %esi
-call scanf
-ret
-
-doAction:
-#finit
-#fldcw FPUControlWord
-#flds a
-#fadds b
-
-finish:
-mov $0, %eax
-call exit
