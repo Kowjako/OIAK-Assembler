bb67f7cfd8c6c38750d8423aa93754feaf08a9f1
Uladzimir Kaviaka <257276@student.pwr.edu.pl>
2021-04-23 23:44:41 +0200

    Re-writed operations for 32bit linux

--- Laboratorium2/kalkulator.s
+++ Laboratorium2/kalkulator.s
@@ -1,71 +1,61 @@
-.global main
-
-cut1: .short 0x200 #najblizsze
-cut2: .short 0x600 #dol
-cut3: .short 0xA00 #gora
-cut4: .short 0xE00 #obciecie
-
-.macro endl
-    print_s endline
-.endm
+cut1: .short 0x03F #najblizsze
+cut2: .short 0x43F #dol
+cut3: .short 0x83F #gora
+cut4: .short 0xC3F #obciecie
 
 .macro print_s string
-    mov $0, %eax
-    mov $format_s, %edi
-    mov	$\string, %esi
+    pushl $\string
     call printf
-.endm
-
-.macro print_d double
-    subq $8, %rsp
-    mov $1, %eax
-    mov $format_d, %edi
-    movsd \double, %xmm0
-    call printf
-    addq $8, %rsp
-.endm
-
-.macro scan_d register
-    subq $8, %rsp
-    mov $0, %eax
-    mov $format_d, %edi
-    mov $\register, %esi
-    call scanf
-    addq $8, %rsp
-.endm
-
-.macro scan_i register
-   subq $8, %rsp
-   mov $0, %eax
-   mov $format_i, %edi
-   mov $\register, %esi
-   call scanf
-   addq $8, %rsp
 .endm
 
 .macro add_d			#makro do wykonania dodawania FPU
-   fldl first_d
-   faddl second_d
+   fld first_d
+   fld second_d
+   faddp
+   push %eax
+   push %eax
+   fstpl (%esp)
+   pushl $output
+   call printf
+   pushl $0
 .endm
 
 .macro sub_d			#makro do wykonania odejmowania FPU
-    fldl first_d
-    fsubl second_d
+    fld first_d
+    fld second_d
+    fsubp
+    push %eax
+    push %eax
+    fstpl (%esp)
+    pushl $output
+    call printf
+    pushl $0
 .endm
 
 .macro mult_d			#makro do wykonania mnozenia FPU
-    fldl first_d
-    fmull second_d
+    fld first_d
+    fld second_d
+    fmulp
+    push %eax
+    push %eax
+    fstpl (%esp)
+    pushl $output
+    call printf
+    pushl $0
 .endm
 
 .macro div_d			#makro do wykonania dzielenia FPU
-    fldl first_d
-    fdivl second_d
+    fld first_d
+    fld second_d
+    fdivp
+    push %eax
+    push %eax
+    fstpl (%esp)
+    pushl $output
+    call printf
+    pushl $0
 .endm
 
-.macro take_d double	#makro do pobierania wyniku po operacji FPU
-    fstl \double
-.endm
 
 .macro set_round_cut    #zaokraglanie przez obciecie
     fldcw cut4
@@ -88,35 +78,43 @@ cut4: .short 0xE00 #obciecie
     msg2: .ascii "Podaj druga liczbe\n\0"
     menu: .ascii "Wybierz typ operacji\n1. Dodawanie\n2. Odejmowanie\n3. Dzielenie\n4. Mnozenie\n\0"
     control: .ascii "Typ zaokraglania\n1. Do zera\n2. W gore\n3. W dol\n4. Do najblizszej\n\0"
-    output: .ascii "Wynik\n\0"
+    output: .ascii "Wynik %f\n\0"
 
-    first_d: .double 1.0
-    second_d: .double 2.0
-    output_d: .double 0.0
-
+    first_d: .float 0.0
+    second_d: .float 0.0
     operation: .int 0			#zmienna dla przechowywania wybranej operacji
     control_operation: .int 0	#zmienna dla przechowywania wybranego zaokraglania
 
-    format_d: .ascii "%lf\0"	#format wypisywania double
-    format_s: .ascii "%s\0"		#format wypisywania string
+    format_d: .ascii "%f\0"	    #format wypisywania double
     format_i: .ascii "%i\0"		#format wypisywania calkowitej liczby
 
-    endline: .ascii "\n\0"
+
 .text
-main:
+.globl _start
+
+_start:
 	finit				#inicjalizacja jednostki FPU
 
     print_s msg1
-    scan_d first_d
+   	pushl $first_d
+   	pushl $format_d
+   	call scanf
+
 
     print_s msg2
-    scan_d second_d
+    pushl $second_d
+   	pushl $format_d
+   	call scanf
 
     print_s menu
-    scan_i operation
+    pushl $operation
+   	pushl $format_i
+   	call scanf
 
     print_s control
-    scan_i control_operation
+    pushl $control_operation
+   	pushl $format_i
+   	call scanf
 
     cmpb $1, control_operation
     je cut
@@ -162,7 +160,4 @@ mult:
     mult_d
     jmp end
 end:
-    take_d output_d
-    print_s output
-    print_d output_d
-    endl
+    call exit
