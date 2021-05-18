1d89999375870a7f26346f5212ead2d9ec18205d
Uladzimir Kaviaka <257276@student.pwr.edu.pl>
2021-05-18 20:47:32 +0200

    Worked parameters sending from C to asm

--- Laboratorium4/encoder.s
+++ Laboratorium4/encoder.s
@@ -11,8 +11,12 @@ encoder:
 push %ebp
 mov %esp, %ebp
 
-mov 8(%ebp), %eax 	#w eax adres gdzie zaczyna sie tablica pixeli, w edx ilosc pixeli naszego bmp
-mov %edx, %esi		#zapisujemy nasza liczbe pixeli
+#pobieramy ze stosu nasze parametry z funkcji C#
+
+mov 8(%ebp), %esi 	#w eax adres gdzie zaczyna sie tablica pixeli
+mov 12(%ebp), %edi	#zapisujemy nasza liczbe pixeli
+mov 16(%ebp), %ebx  #pobieramy nasz tekst
+mov 20(%ebp), %eax  #dlugosc tekstu
 
 finish:
 pop %ebp
